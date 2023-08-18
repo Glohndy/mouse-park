5a6f202b8a716dda06dc768d3f64d99fa1540ff9
jean-marie <jeanmariekouassi70@gmail.com>
2023-08-18 02:27:03 +0000

    premiere version version du site mouse-park

---
+++ js4/script.js
@@ -0,0 +1,28 @@
+$(function () {
+    
+    $('#contact-form').submit(function(e) {
+        e.preventDefault();
+        $('.comments').empty();
+        var postdata = $('#contact-form').serialize();
+        
+        $.ajax({
+            type: 'POST',
+            url: 'php/contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+                 
+                if(json.isSuccess) {
+                    $('#contact-form').append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>");
+                    $('#contact-form')[0].reset();
+                } else {
+
+                    $('#name + .comments').html(json.nameError);
+                    $('#email + .comments').html(json.emailError);
+                    $('#message + .comments').html(json.messageError);
+                }                
+            }
+        });
+    });
+
+})
